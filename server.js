ff52a75e50f7fdbfc9b7a72a46961a4acfbf67aa
shyakadev <shyakadev@gmail.com>
2022-03-31 15:35:46 +0200

    fix: removed upload destination because vercel is read only

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var cors = require("cors");
 require("dotenv").config();
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: null });
 
 var app = express();
 
